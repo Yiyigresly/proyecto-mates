94666f45c5f6d30c5990460881e0d58407261481
Yiyigresly <keorza@gmail.com>
2023-05-25 11:33:42 +0200

    modificacion derivadas desde rama feature

--- derivadas.py
+++ derivadas.py
@@ -1,3 +1,3 @@
 print('esto es una derivada y no es una integral')
 print('modificaciones extrassssss')
-
+print('modificaciones desde rama feature algoritmos , traidas con rebase y añadiendo algo')
